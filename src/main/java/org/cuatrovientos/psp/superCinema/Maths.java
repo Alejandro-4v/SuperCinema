f872945ec005354de453792ac0898e4e24391b2e
Alejacks <alejandrolopez2048@gmail.com>
2025-12-17 19:08:24 +0100

    feat: Static method for generating a random int from a range created

--- src/main/java/org/cuatrovientos/psp/superCinema/Maths.java
+++ src/main/java/org/cuatrovientos/psp/superCinema/Maths.java
@@ -1,4 +1,13 @@
 package org.cuatrovientos.psp.superCinema;
 
+import java.util.Random;
+
 public class Maths {
+
+    private static final Random RANDOM = new Random();
+
+    public static int randomIntBetweenRangeInclusive(int start, int bound) {
+        return RANDOM.nextInt(bound - start + 1) + start;
+    }
+
 }
