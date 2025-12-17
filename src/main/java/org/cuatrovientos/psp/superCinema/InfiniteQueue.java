cb1d060fcf4600b13f42eb34865c8a1b6f486ff4
Alejacks <alejandrolopez2048@gmail.com>
2025-12-17 20:09:10 +0100

    feat: InfiniteQueue methods for stopping the queue, adding cinephiles, popping the first of the queue added. Property for checking if the queue is still waiting for cinephiles

--- src/main/java/org/cuatrovientos/psp/superCinema/InfiniteQueue.java
+++ src/main/java/org/cuatrovientos/psp/superCinema/InfiniteQueue.java
@@ -1,16 +1,36 @@
 package org.cuatrovientos.psp.superCinema;
 
+import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class InfiniteQueue {
 
     private final BlockingQueue<Cinephile> cinephilesWaitingInLine = new LinkedBlockingQueue<>();
 
     private final String IDENTIFIER;
+    private final AtomicBoolean isStillWaitingForCinephiles = new AtomicBoolean(true);
 
     public InfiniteQueue(String name) {
         this.IDENTIFIER = name;
+    }
+
+    public void addCinephilesToQueue(ArrayList<Cinephile> cinephilesToCheckIn) {
+        System.out.printf("%s - %s cinephiles added to queue", this.IDENTIFIER, cinephilesToCheckIn.size());
+        cinephilesWaitingInLine.addAll(cinephilesToCheckIn);
+    }
+
+    public Cinephile getWaitingCinephile() throws InterruptedException {
+        return cinephilesWaitingInLine.take();
+    }
+
+    public void closeQueue() {
+        isStillWaitingForCinephiles.set(false);
+    }
+
+    public boolean isStillWaitingForCinephiles() {
+        return isStillWaitingForCinephiles.get();
     }
 
 }
