ef0a12394ec8000c839f50e3f91a8d03ddeaaea9
Alejacks <alejandrolopez2048@gmail.com>
2025-12-18 12:39:18 +0100

    feat: Business logic for the BoxOffice created with all methods needed for this logic

--- src/main/java/org/cuatrovientos/psp/superCinema/BoxOffice.java
+++ src/main/java/org/cuatrovientos/psp/superCinema/BoxOffice.java
@@ -1,21 +1,70 @@
 package org.cuatrovientos.psp.superCinema;
 
-public class BoxOffice {
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class BoxOffice implements Runnable {
 
     private final int MIN_TIME_IN_SECONDS_FOR_SELLING_TICKET = 20;
     private final int MAX_TIME_IN_SECONDS_FOR_SELLING_TICKET = 30;
 
     private final int TIME_IN_MINUTES_FOR_OPENING_EARLIER = 30;
 
-    private final InfiniteQueue INFINITEQUEUE1;
-    private final InfiniteQueue INFINITEQUEUE2;
-
     private final String IDENTIFIER;
+    private final InfiniteQueue INFINITEQUEUE;
 
-    public BoxOffice(String name, InfiniteQueue INFINITEQUEUE1, InfiniteQueue INFINITEQUEUE2) {
+    private long openedAtTimestap;
+
+    private final AtomicInteger soldTickets = new AtomicInteger(0);
+
+    public BoxOffice(String name, InfiniteQueue INFINITEQUEUE) {
         this.IDENTIFIER = name;
-        this.INFINITEQUEUE1 = INFINITEQUEUE1;
-        this.INFINITEQUEUE2 = INFINITEQUEUE2;
+        this.INFINITEQUEUE = INFINITEQUEUE;
+
+        this.openedAtTimestap = System.currentTimeMillis();
+    }
+
+    @Override
+    public void run() {
+
+        while (isBoxOfficeStillOpen()) {
+            try {
+                Cinephile waitingCinephile = INFINITEQUEUE.getWaitingCinephile();
+                if (waitingCinephile != null) {
+                    System.out.printf("%s - Started selling tickets to %s\n", this.IDENTIFIER, waitingCinephile.getIDENTIFIER());
+                    Thread.sleep((long) Maths.randomIntBetweenRangeInclusive(MIN_TIME_IN_SECONDS_FOR_SELLING_TICKET, MAX_TIME_IN_SECONDS_FOR_SELLING_TICKET) * Maths.SCALE_FOR_MILLISECONDS_FROM_SECONDS);
+                    System.out.printf("%s - Finished selling tickets to %s\n", this.IDENTIFIER, waitingCinephile.getIDENTIFIER());
+                    soldTickets.incrementAndGet();
+                }
+            } catch (InterruptedException e) {
+                closeBoxOffice();
+            }
+        }
+
+    }
+
+    public void closeBoxOffice() {
+        closeQueue();
+        System.out.printf("Box Office closed. Total sold tickets in this box office: %s\n", soldTickets.get());
+    }
+
+    private void closeQueue() {
+        INFINITEQUEUE.closeQueue();
+    }
+
+    private long getTimeFromOpeningInMiliseconds() {
+        return System.currentTimeMillis() - openedAtTimestap;
+    }
+
+    private boolean checkIfTheFilmHasStarted() {
+        return getTimeFromOpeningInMiliseconds() > TIME_IN_MINUTES_FOR_OPENING_EARLIER * Maths.SCALE_FOR_MILLISECONDS_FROM_MINUTES;
+    }
+
+    public boolean isBoxOfficeStillOpen() {
+        return !checkIfTheFilmHasStarted();
+    }
+
+    public int getSoldTickets() {
+        return soldTickets.get();
     }
 
 }
