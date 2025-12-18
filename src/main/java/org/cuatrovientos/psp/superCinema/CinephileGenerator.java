71206e5dcc06235f90eaa67014eff9d9ceceed6f
Alejacks <alejandrolopez2048@gmail.com>
2025-12-18 12:20:04 +0100

    feat: Changed constants in order to make the time taken for actions change if the time conversion ratios in Maths.java are modified

--- src/main/java/org/cuatrovientos/psp/superCinema/CinephileGenerator.java
+++ src/main/java/org/cuatrovientos/psp/superCinema/CinephileGenerator.java
@@ -1,25 +1,30 @@
 package org.cuatrovientos.psp.superCinema;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CinephileGenerator implements Runnable {
 
     private final int MIN_CINEPHILES_PER_MINUTE_RATIO = 10;
     private final int MAX_CINEPHILES_PER_MINUTE_RATIO = 15;
-    private final int TIME_IN_MILLISECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILES = 60000;
+    private final int TIME_IN_SECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILES = 60;
+
+    private final int TIME_IN_MILLISECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILES = TIME_IN_SECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILES * Maths.SCALE_FOR_MILLISECONDS_FROM_SECONDS;
 
     private final String IDENTIFIER;
     private final InfiniteQueue INFINITEQUEUE;
 
+    private final AtomicInteger currentCinephileId = new AtomicInteger(0);
+
     public CinephileGenerator(String name, InfiniteQueue INFINITEQUEUE) {
-        this.IDENTIFIER = String.format("%s-cinephileGenerator", name);
+        this.IDENTIFIER = name;
         this.INFINITEQUEUE = INFINITEQUEUE;
     }
 
     @Override
     public void run() {
 
-        System.out.printf("%s - Started generating cinephiles", this.IDENTIFIER);
+        System.out.printf("%s - Started generating cinephiles\n", this.IDENTIFIER);
 
         while (INFINITEQUEUE.isStillWaitingForCinephiles()) {
             int cinephilesPerMinute = Maths.randomIntBetweenRangeInclusive(MIN_CINEPHILES_PER_MINUTE_RATIO, MAX_CINEPHILES_PER_MINUTE_RATIO);
@@ -28,13 +33,16 @@ public class CinephileGenerator implements Runnable {
                 Thread.sleep(TIME_IN_MILLISECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILES);
                 ArrayList<Cinephile> cinephilesToAddToQueue = new ArrayList<>();
                 for (int i = 0; i < cinephilesPerMinute; i++) {
-                    cinephilesToAddToQueue.add(new Cinephile(String.format("%s-%s", IDENTIFIER, i)));
+                    cinephilesToAddToQueue.add(new Cinephile(String.format("%s-cinephile-%s", IDENTIFIER, currentCinephileId.getAndIncrement())));
                 }
                 INFINITEQUEUE.addCinephilesToQueue(cinephilesToAddToQueue);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                closeCinephileGenerator();
             }
         }
+    }
 
+    private void closeCinephileGenerator() {
+        System.out.printf("%s - Queue closed", this.IDENTIFIER);
     }
 }
