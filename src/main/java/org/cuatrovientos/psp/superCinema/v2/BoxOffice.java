b5690d4d4f6e529f3b513e0ebb0421f6c2dda3cf
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-22 23:39:33 +0100

    feat: BoxOffice logic updated and adapted to this new style of X queues per BoxOffice so it checks the one with the most cinephiles waiting and takes the next cinephile from there

--- src/main/java/org/cuatrovientos/psp/superCinema/v2/BoxOffice.java
+++ src/main/java/org/cuatrovientos/psp/superCinema/v2/BoxOffice.java
@@ -5,15 +5,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class BoxOffice implements Runnable {
 
     private final String IDENTIFIER;
-    private final InfiniteQueue INFINITEQUEUE;
+    private final FiniteQueue[] FINITEQUEUES;
 
     private final long openedAtTimestamp;
 
     private final AtomicInteger soldTickets = new AtomicInteger(0);
 
-    public BoxOffice(String name, InfiniteQueue INFINITEQUEUE) {
+    public BoxOffice(String name, FiniteQueue[] FINITEQUEUES) {
         this.IDENTIFIER = name;
-        this.INFINITEQUEUE = INFINITEQUEUE;
+        this.FINITEQUEUES = FINITEQUEUES;
 
         this.openedAtTimestamp = System.currentTimeMillis();
     }
@@ -23,11 +23,11 @@ public class BoxOffice implements Runnable {
 
         while (isBoxOfficeStillOpen()) {
             try {
-                Cinephile waitingCinephile = INFINITEQUEUE.getWaitingCinephile();
-                if (waitingCinephile != null) {
-                    System.out.printf("%s - Started selling tickets to %s\n", this.IDENTIFIER, waitingCinephile.getIDENTIFIER());
+                Cinephile nextCinephile = getNextCinephile();
+                if (nextCinephile != null) {
+                    System.out.printf("%s - Started selling tickets to %s\n", this.IDENTIFIER, nextCinephile.getIDENTIFIER());
                     Thread.sleep((long) Maths.randomIntBetweenRangeInclusive(Finals.MIN_TIME_IN_SECONDS_FOR_SELLING_TICKET, Finals.MAX_TIME_IN_SECONDS_FOR_SELLING_TICKET) * Maths.SCALE_FOR_MILLISECONDS_FROM_SECONDS);
-                    System.out.printf("%s - Finished selling tickets to %s\n", this.IDENTIFIER, waitingCinephile.getIDENTIFIER());
+                    System.out.printf("%s - Finished selling tickets to %s\n", this.IDENTIFIER, nextCinephile.getIDENTIFIER());
                     soldTickets.incrementAndGet();
                 }
             } catch (InterruptedException e) {
@@ -38,21 +38,9 @@ public class BoxOffice implements Runnable {
 
     }
 
-    public void closeBoxOffice() {
-        closeQueue();
-        System.out.printf("Box Office closed. Total sold tickets in this box office: %s\n", soldTickets.get());
-    }
-
-    private void closeQueue() {
-        INFINITEQUEUE.closeQueue();
-    }
-
-    private long getTimeFromOpeningInMilliseconds() {
-        return System.currentTimeMillis() - openedAtTimestamp;
-    }
-
-    private boolean checkIfTheFilmHasStarted() {
-        return getTimeFromOpeningInMilliseconds() > Finals.TIME_IN_MILLISECONDS_FOR_OPENING_EARLIER;
+    private void closeBoxOffice() {
+        closeQueues();
+        System.out.printf("Box Office (%s) closed. Total sold tickets in this box office: %s\n", this.IDENTIFIER, soldTickets.get());
     }
 
     public boolean isBoxOfficeStillOpen() {
@@ -61,6 +49,40 @@ public class BoxOffice implements Runnable {
 
     public int getSoldTickets() {
         return soldTickets.get();
+    }
+
+    private void closeQueues() {
+        for (FiniteQueue finiteQueue : FINITEQUEUES) {
+            finiteQueue.closeQueue();
+        }
+    }
+
+    private Cinephile getNextCinephile() throws InterruptedException {
+        return getFiniteQueueWithMoreCinephilesWaiting().getWaitingCinephile();
+    }
+
+    private FiniteQueue getFiniteQueueWithMoreCinephilesWaiting() {
+        if (FINITEQUEUES.length == 1) {
+            return FINITEQUEUES[0];
+        }
+
+        FiniteQueue finiteQueueWithMoreCinephilesWaiting = FINITEQUEUES[0];
+
+        for (FiniteQueue finiteQueue : FINITEQUEUES) {
+            if (finiteQueue.getFreeSpacesInQueue() > finiteQueueWithMoreCinephilesWaiting.getFreeSpacesInQueue()) {
+                finiteQueueWithMoreCinephilesWaiting = finiteQueue;
+            }
+        }
+
+        return finiteQueueWithMoreCinephilesWaiting;
+    }
+
+    private long getTimeFromOpeningInMilliseconds() {
+        return System.currentTimeMillis() - openedAtTimestamp;
+    }
+
+    private boolean checkIfTheFilmHasStarted() {
+        return getTimeFromOpeningInMilliseconds() > Finals.TIME_IN_MILLISECONDS_FOR_OPENING_EARLIER;
     }
 
 }
