731625795785651656e4cfd26d131ad607d69083
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-23 00:13:46 +0100

    refactor: All methods refactor on Cinema with new verbose names

--- src/main/java/org/cuatrovientos/psp/superCinema/v2/Cinema.java
+++ src/main/java/org/cuatrovientos/psp/superCinema/v2/Cinema.java
@@ -9,45 +9,32 @@ public class Cinema {
     private static final BoxOffice[] BOX_OFFICES = new BoxOffice[Finals.NUMBER_OF_BOX_OFFICES];
     private static final Thread[] BOX_OFFICES_THREADS = new Thread[Finals.NUMBER_OF_BOX_OFFICES];
 
+    private static CinephileGenerator MAIN_CINEPHILEGENERATOR;
+    private static Thread MAIN_CINEPHILEGENERATOR_THREAD;
+
     public static void main(String[] args) throws InterruptedException {
 
         Finals.checkFinals();
 
-        for (int queueIndex = 0; queueIndex < FINITE_QUEUES.length; queueIndex++) {
-            FiniteQueue newQueue = new FiniteQueue(String.format("queue-%s", queueIndex));
-            FINITE_QUEUES[queueIndex] = newQueue;
-        }
+        generateQueues();
 
-        CinephileGenerator MAIN_CINEPHILEGENERATOR = new CinephileGenerator("MAIN_CINEPHILEGENERATOR", FINITE_QUEUES);
-        Thread MAIN_CINEPHILEGENERATOR_THREAD = new Thread(MAIN_CINEPHILEGENERATOR);
-        MAIN_CINEPHILEGENERATOR_THREAD.start();
+        generateMainCinephileGenerator();
 
-        for (int currentBoxOfficeIndex = 0; currentBoxOfficeIndex < BOX_OFFICES.length; currentBoxOfficeIndex++) {
-            FiniteQueue[] finiteQueuesForCurrentBoxOffice = new FiniteQueue[Finals.NUMBER_OF_QUEUES_PER_BOX_OFFICE];
-            for (int queueIndex = 0; queueIndex < finiteQueuesForCurrentBoxOffice.length; queueIndex++) {
-                finiteQueuesForCurrentBoxOffice[queueIndex] = FINITE_QUEUES[currentBoxOfficeIndex * Finals.NUMBER_OF_QUEUES_PER_BOX_OFFICE + queueIndex];
-            }
+        startMainCinephileGenerator();
 
-            BoxOffice newBoxOffice = new BoxOffice(String.format("boxOffice-%s", currentBoxOfficeIndex), finiteQueuesForCurrentBoxOffice);
-            BOX_OFFICES[currentBoxOfficeIndex] = newBoxOffice;
+        assignFiniteQueuesToCorrespondingBoxOffice();
 
-        }
+        generateBoxOfficeThreads();
 
-        for (int currentBoxOfficeThreadIndex = 0; currentBoxOfficeThreadIndex < BOX_OFFICES_THREADS.length; currentBoxOfficeThreadIndex++) {
-            Thread currentBoxOfficeThread = new Thread(BOX_OFFICES[currentBoxOfficeThreadIndex]);
-            BOX_OFFICES_THREADS[currentBoxOfficeThreadIndex] = currentBoxOfficeThread;
-        }
-
-        for (Thread currentBoxOfficeThread : BOX_OFFICES_THREADS) {
-            currentBoxOfficeThread.start();
-        }
+        openAllBoxOffices();
 
         while (!shouldTheCinemaClose()) {
             Thread.sleep(TIME_IN_MILLISECONDS_TO_WAIT_BEFORE_CHECKING_CINEMA_STATUS);
         }
 
         closeAllBoxOffices();
-        MAIN_CINEPHILEGENERATOR_THREAD.interrupt();
+
+        stopMainCinephileGenerator();
 
         System.out.printf("Cinema open! The movie is starting! Total sold tickets: %s\n", getSoldTicketsAmongAllBoxOffices());
     }
@@ -77,10 +64,59 @@ public class Cinema {
         return !isAnyBoxOfficeStillOpen() || allAreTicketsSold();
     }
 
+    private static void generateQueues() {
+        for (int queueIndex = 0; queueIndex < FINITE_QUEUES.length; queueIndex++) {
+            FiniteQueue newQueue = new FiniteQueue(String.format("queue-%s", queueIndex));
+            FINITE_QUEUES[queueIndex] = newQueue;
+        }
+    }
+
+    private static void generateMainCinephileGenerator() {
+        MAIN_CINEPHILEGENERATOR = new CinephileGenerator("MAIN_CINEPHILEGENERATOR", FINITE_QUEUES);
+        MAIN_CINEPHILEGENERATOR_THREAD = new Thread(MAIN_CINEPHILEGENERATOR);
+    }
+
+    private static void startMainCinephileGenerator() {
+        MAIN_CINEPHILEGENERATOR_THREAD.start();
+    }
+
+    private static void assignFiniteQueuesToCorrespondingBoxOffice() {
+        for (int currentBoxOfficeIndex = 0; currentBoxOfficeIndex < BOX_OFFICES.length; currentBoxOfficeIndex++) {
+            FiniteQueue[] finiteQueuesForCurrentBoxOffice = new FiniteQueue[Finals.NUMBER_OF_QUEUES_PER_BOX_OFFICE];
+            for (int currentQueueIndex = 0; currentQueueIndex < finiteQueuesForCurrentBoxOffice.length; currentQueueIndex++) {
+                finiteQueuesForCurrentBoxOffice[currentQueueIndex] = FINITE_QUEUES[getCorrespondingFiniteQueueIndexForBoxOffice(currentBoxOfficeIndex, currentQueueIndex)];
+            }
+
+            BoxOffice newBoxOffice = new BoxOffice(String.format("boxOffice-%s", currentBoxOfficeIndex), finiteQueuesForCurrentBoxOffice);
+            BOX_OFFICES[currentBoxOfficeIndex] = newBoxOffice;
+        }
+    }
+
+    private static void generateBoxOfficeThreads() {
+        for (int currentBoxOfficeThreadIndex = 0; currentBoxOfficeThreadIndex < BOX_OFFICES_THREADS.length; currentBoxOfficeThreadIndex++) {
+            Thread currentBoxOfficeThread = new Thread(BOX_OFFICES[currentBoxOfficeThreadIndex]);
+            BOX_OFFICES_THREADS[currentBoxOfficeThreadIndex] = currentBoxOfficeThread;
+        }
+    }
+
+    private static void openAllBoxOffices() {
+        for (Thread currentBoxOfficeThread : BOX_OFFICES_THREADS) {
+            currentBoxOfficeThread.start();
+        }
+    }
+
     private static void closeAllBoxOffices() {
         for (Thread currentBoxOfficeThread : BOX_OFFICES_THREADS) {
             currentBoxOfficeThread.interrupt();
         }
+    }
+
+    private static void stopMainCinephileGenerator() {
+        MAIN_CINEPHILEGENERATOR_THREAD.interrupt();
+    }
+
+    private static int getCorrespondingFiniteQueueIndexForBoxOffice(int currentBoxOfficeIndex, int queueIndex) {
+        return currentBoxOfficeIndex * Finals.NUMBER_OF_QUEUES_PER_BOX_OFFICE + queueIndex;
     }
 
 }
