94dec7b63142a60e6b363509e00b9b105c3feea9
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-22 23:35:37 +0100

    feat: New logic for CinephileGenerator capable of generating a Cinephile and assigning it to the queue with more free places, printing an error message in case all the queues were full

--- src/main/java/org/cuatrovientos/psp/superCinema/v2/CinephileGenerator.java
+++ src/main/java/org/cuatrovientos/psp/superCinema/v2/CinephileGenerator.java
@@ -1,18 +1,17 @@
 package org.cuatrovientos.psp.superCinema.v2;
 
-import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CinephileGenerator implements Runnable {
 
     private final String IDENTIFIER;
-    private final InfiniteQueue INFINITEQUEUE;
+    private final FiniteQueue[] finiteQueues;
 
     private final AtomicInteger currentCinephileId = new AtomicInteger(0);
 
-    public CinephileGenerator(String name, InfiniteQueue INFINITEQUEUE) {
+    public CinephileGenerator(String name, FiniteQueue[] INFINITEQUEUES) {
         this.IDENTIFIER = name;
-        this.INFINITEQUEUE = INFINITEQUEUE;
+        this.finiteQueues = INFINITEQUEUES;
     }
 
     @Override
@@ -20,24 +19,50 @@ public class CinephileGenerator implements Runnable {
 
         System.out.printf("%s - Started generating cinephiles\n", this.IDENTIFIER);
 
-        while (INFINITEQUEUE.isStillWaitingForCinephiles()) {
-            int cinephilesPerMinute = Maths.randomIntBetweenRangeInclusive(Finals.MIN_CINEPHILES_PER_MINUTE_RATIO, Finals.MAX_CINEPHILES_PER_MINUTE_RATIO);
-
+        while (isAnyFiniteQueueWaitingForCinephiles()) {
             try {
-                Thread.sleep(Finals.TIME_IN_MILLISECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILES);
-                ArrayList<Cinephile> cinephilesToAddToQueue = new ArrayList<>();
-                for (int i = 0; i < cinephilesPerMinute; i++) {
-                    cinephilesToAddToQueue.add(new Cinephile(String.format("%s-cinephile-%s", IDENTIFIER, currentCinephileId.getAndIncrement())));
-                }
-                INFINITEQUEUE.addCinephilesToQueue(cinephilesToAddToQueue);
+                Thread.sleep(Finals.TIME_IN_MILLISECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILE);
+
+                Cinephile newCinephile = new Cinephile(String.format("%s-cinephile-%s", IDENTIFIER, currentCinephileId.getAndIncrement()));
+
+                FiniteQueue finiteQueueToAddTheCinephile = getFiniteQueueWithMoreRemainingPlaces();
+
+                finiteQueueToAddTheCinephile.addCinephileToQueue(newCinephile);
+
             } catch (InterruptedException e) {
                 closeCinephileGenerator();
                 break;
+            } catch (IllegalStateException e) {
+                System.out.println("Queue is full, the Cinephile walks home sadly because he won't get to see Avatar 2");
             }
         }
     }
 
     private void closeCinephileGenerator() {
-        System.out.printf("%s - Queue closed\n", this.IDENTIFIER);
+        System.out.printf("%s - CinephileGenerator closed\n", this.IDENTIFIER);
     }
+
+    private boolean isAnyFiniteQueueWaitingForCinephiles() {
+        for (FiniteQueue finiteQueue : finiteQueues) {
+            if (finiteQueue.isStillWaitingForCinephiles()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private FiniteQueue getFiniteQueueWithMoreRemainingPlaces() {
+        if (finiteQueues.length == 1) {
+            return finiteQueues[0];
+        }
+
+        FiniteQueue finiteQueueWithMoreRemainingPlaces = finiteQueues[0];
+        for (FiniteQueue finiteQueue : finiteQueues) {
+            if (finiteQueue.getFreeSpacesInQueue() > finiteQueueWithMoreRemainingPlaces.getFreeSpacesInQueue()) {
+                finiteQueueWithMoreRemainingPlaces = finiteQueue;
+            }
+        }
+        return finiteQueueWithMoreRemainingPlaces;
+    }
+
 }
