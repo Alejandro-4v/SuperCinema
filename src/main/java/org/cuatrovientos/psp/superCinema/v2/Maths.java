701acd91089d3f2b3703442da97de446ca6710db
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-22 23:52:34 +0100

    fix: Maths scales for conversion changed back to real scales (debugging scales are now commented)

--- src/main/java/org/cuatrovientos/psp/superCinema/v2/Maths.java
+++ src/main/java/org/cuatrovientos/psp/superCinema/v2/Maths.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Maths {
 
-    public static final int SCALE_FOR_MILLISECONDS_FROM_SECONDS = 1; // DEBUG
-    public static final int SCALE_FOR_MILLISECONDS_FROM_MINUTES = 600; // DEBUG
-//    protected static final int SCALE_FOR_MILLISECONDS_FROM_SECONDS = 1000;
-//    protected static final int SCALE_FOR_MILLISECONDS_FROM_MINUTES = 60000;
+//    protected static final int SCALE_FOR_MILLISECONDS_FROM_SECONDS = 1; // DEBUG
+//    protected static final int SCALE_FOR_MILLISECONDS_FROM_MINUTES = 600; // DEBUG
+    protected static final int SCALE_FOR_MILLISECONDS_FROM_SECONDS = 1000;
+    protected static final int SCALE_FOR_MILLISECONDS_FROM_MINUTES = 60000;
 
     private static final Random RANDOM = new Random();
 
