d97b50f644843eefc0e4c224a82e16f27da8d79c
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-22 23:29:07 +0100

    feat: Added comments for the program parameters in order to make the configuration easier with comments and explanations

--- src/main/java/org/cuatrovientos/psp/superCinema/v2/Finals.java
+++ src/main/java/org/cuatrovientos/psp/superCinema/v2/Finals.java
@@ -2,9 +2,10 @@ package org.cuatrovientos.psp.superCinema.v2;
 
 public class Finals {
 
-    protected static final int MIN_CINEPHILES_PER_MINUTE_RATIO = 10; // Cinéfilos mínimos por minuto
-    protected static final int MAX_CINEPHILES_PER_MINUTE_RATIO = 15; // Cinéfilos máximos por minuto
-    protected static final int TIME_IN_SECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILES = 60; // Tiempo que se tarda en generar nuevos cinéfilos
+    // Aquí no habrá un ratio de cinéfilos por X cantidad de tiempo, pues las colas ahora son finitas.
+    // En las colas esta vez se podrá configurar cada cuanto llega un nuevo cinéfilo
+    // De no haber espacio el cinéfilo no entrará a la cola y marchará triste por no poder ver Avatar 2
+    protected static final int TIME_IN_SECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILE = 15; // Tiempo que se tarda en generar un nuevo cinéfilos
 
     protected static final int MIN_TIME_IN_SECONDS_FOR_SELLING_TICKET = 20; // Tiempo mínimo que tardará un taquillero en vender una entrada
     protected static final int MAX_TIME_IN_SECONDS_FOR_SELLING_TICKET = 30; // Tiempo máximo que tardará un taquillero en vender una entrada
@@ -16,6 +17,7 @@ public class Finals {
     protected static final int NUMBER_OF_QUEUES_PER_BOX_OFFICE = 1; // Colas por taquilla
     protected static final int MAX_CINEPHILES_PER_QUEUE = 10; // Tamaño máximo de las colas de la taquilla
 
-    protected static final int TIME_IN_MILLISECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILES = TIME_IN_SECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILES * Maths.SCALE_FOR_MILLISECONDS_FROM_SECONDS; // No tocar (esto es para pasar de segundos a milisegundos)
+    protected static final int TIME_IN_MILLISECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILE = TIME_IN_SECONDS_TO_SLEEP_UNTIL_NEW_CINEPHILE * Maths.SCALE_FOR_MILLISECONDS_FROM_SECONDS; // No tocar (esto es para pasar de segundos a milisegundos)
     protected static final int TIME_IN_MILLISECONDS_FOR_OPENING_EARLIER = TIME_IN_MINUTES_FOR_OPENING_EARLIER * Maths.SCALE_FOR_MILLISECONDS_FROM_MINUTES; // No tocar (esto es para pasar de segundos a milisegundos)
+
 }
