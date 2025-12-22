22e1fc041a13bcd4da5ed03b1a89dea0972a3448
Alejandro López Domínguez <alejandrolopez2048@gmail.com>
2025-12-22 23:30:43 +0100

    feat: InfiniteQueue changed to FiniteQueue with new logic. Now using ArrayBlockingQueue instead of LinkedBlockingQueue

--- src/main/java/org/cuatrovientos/psp/superCinema/v2/FiniteQueue.java
+++ src/main/java/org/cuatrovientos/psp/superCinema/v2/FiniteQueue.java
@@ -1,24 +1,27 @@
 package org.cuatrovientos.psp.superCinema.v2;
 
-import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class InfiniteQueue {
+public class FiniteQueue {
 
-    private final BlockingQueue<Cinephile> cinephilesWaitingInLine = new LinkedBlockingQueue<>();
+    private final BlockingQueue<Cinephile> cinephilesWaitingInLine = new ArrayBlockingQueue<>(Finals.MAX_CINEPHILES_PER_QUEUE);
 
     private final String IDENTIFIER;
     private final AtomicBoolean isStillWaitingForCinephiles = new AtomicBoolean(true);
 
-    public InfiniteQueue(String name) {
+    public FiniteQueue(String name) {
         this.IDENTIFIER = name;
     }
 
-    public void addCinephilesToQueue(ArrayList<Cinephile> cinephilesToCheckIn) {
-        System.out.printf("%s - %s cinephiles added to queue \n", this.IDENTIFIER, cinephilesToCheckIn.size());
-        cinephilesWaitingInLine.addAll(cinephilesToCheckIn);
+    public int getFreeSpacesInQueue() {
+        return cinephilesWaitingInLine.remainingCapacity();
+    }
+
+    public void addCinephileToQueue(Cinephile cinephileToCheckIn) {
+        System.out.printf("%s - cinephile added to queue \n", this.IDENTIFIER);
+        cinephilesWaitingInLine.add(cinephileToCheckIn);
     }
 
     public Cinephile getWaitingCinephile() throws InterruptedException {
